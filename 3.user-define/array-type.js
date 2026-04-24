4cd4fefa80f31d81967d52e6bce515092a1a4014
Rong <rongrubaiya@gmail.com>
2026-04-24 17:13:21 +0600

    array type

---
+++ 3.user-define/array-type.js
@@ -0,0 +1,8 @@
+"use strict";
+// let userNames : string[];
+let userNames;
+userNames = ["Bob", "Alice", "Charlie"];
+console.log(userNames.sort());
+// let maltipleTypes : (string | number)[];
+// maltipleTypes = ["Hello", 42, "World", 3.14];
+// console.log(maltipleTypes)
---
+++ 3.user-define/array-type.ts
@@ -0,0 +1,12 @@
+// let userNames : string[];
+let userNames : Array<string>;
+
+userNames = ["Bob", "Alice", "Charlie"];
+
+console.log(userNames.sort());
+
+// let maltipleTypes : (string | number)[];
+
+// maltipleTypes = ["Hello", 42, "World", 3.14];
+
+// console.log(maltipleTypes)
